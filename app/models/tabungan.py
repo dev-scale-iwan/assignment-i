fc6cfbf88733307c531c2b3d14eb9a30f507f72f
iwan susanto <iwandevapps@gmail.com>
2026-02-07 14:34:54 +0700

    alemic setting

--- app/models/tabungan.py
+++ app/models/tabungan.py
@@ -5,4 +5,4 @@ class Tabungan(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=lambda: uuid.uuid4(), primary_key=True)
     nama: str = Field(default=None)
     jumlah: float = Field(default=0.0)
-    descripsi: str = Field(default=None)
+    description: str = Field(default=None)
